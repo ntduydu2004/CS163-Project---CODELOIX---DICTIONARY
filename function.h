a143355a9e00cbc9b0b0c1f950b9f1b87169fca2
PhucHoang <phucchu390@gmail.com>
2022-06-24 16:39:51 +0700

    Change Function Call

--- function.h
+++ function.h
@@ -1,5 +1,11 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
 #include <iomanip>
 
+void loadHistoryFile(vector<string> &hisSearch);
 void Mainmenu(int type);
 void runProgram(int type);
+void displayHistory(vector<string> hisSearch);
+void saveHistory(vector<string> hisSearch);
